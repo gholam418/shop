16e1d4d0fed6b91a13a5fff0b052db9776ce1f27
gholam418@gmail.com <gholam418@gmail.com>
2025-10-16 13:31:01 +0330

    حذف فایل های ناخواسته مثل shopvenv

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from  django.http import JsonResponse
 # Create your views here.
+class Register(APIView):
+    @staticmethod
+    def get(self , request):
+        return JsonResponse({"name":"Ali"})
+    
+    
+
+
+class Login(APIView):
+    pass
+
+
+class ForgetPass(APIView):
+    pass
+
+
+class UpdateProfile(APIView):
+    pass
+
